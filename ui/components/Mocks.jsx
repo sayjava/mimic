5d67ee49809b137ba778ad1db5ded338d03f20ad
Raymond Ottun <sayjava@gmail.com>
2022-09-06 09:12:00 +0100

    feat: delete mock

--- ui/components/Mocks.jsx
+++ ui/components/Mocks.jsx
@@ -1,40 +1,64 @@
-import React from 'react'
-import { useMocksState } from '../providers/mocks.jsx'
+import React, { useState, useEffect } from 'react'
 
 const noop = () => {}
 
 const MocksTable = ({ onSelect = noop }) => {
-    const { mocks, error, busy } = useMocksState()
-    
-    if(busy) {
-        return <div>Loading up mocks</div>
+    const [state, setState] = useState({ busy: false, error: null, mocks: []})
+
+    const deleteMock = async (mock) => {
+        const res = await fetch(`${process.env.apiPath}/api/mocks/${mock.id}`, {
+            method: 'DELETE'
+        })
+
+        if(!res.ok) {
+            console.error(`Could not delete ${mock.id}`)
+        } else {
+            loadMocks()
+        }
     }
 
-    if(error) {
-        return <div>
-            Error: {JSON.stringify(error)}
-        </div>
-    }
+    const loadMocks = async () => {
+        try {
+            setState({ mocks: [], error: null, busy: true });
+            const res = await fetch(`${process.env.apiPath}/api/mocks`);
+            const mocks = await res.json();
+            setState({ mocks, error: null, busy: false });
+        } catch (error) {
+            setState({ mocks: [], error, busy: false });
+        }
+      
+    };
 
-  if (mocks) {
+    useEffect(loadMocks, [])
+
     return (
       <div>
-        <h3>The Dashboard</h3>
+        {state.error && <div>
+            Error: {state.error.message}
+            </div>}
+            {state.busy && <div>
+            Busy working
+            </div>}
+            <div>Mocks</div>
         <table>
           <thead>
             <tr>
               <th>method</th>
               <th>path</th>
               <th>status</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {mocks.map((mock) => {
+            {state.mocks.map((mock) => {
               return (
                 <tr key={mock.id} onClick={() => onSelect(mock)}>
                   <td>{mock.request.method}</td>
                   <td>{mock.request.path}</td>
                   <td>{mock.response.status}</td>
+                  <td>
+                    <button onClick={() => deleteMock(mock)}>Delete</button>
+                  </td>
                 </tr>
               );
             })}
@@ -42,7 +66,6 @@ const MocksTable = ({ onSelect = noop }) => {
         </table>
       </div>
     );
-  }
 }   
 
 export default MocksTable
