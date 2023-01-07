948e00ea33fa341b2d99bb1708fb39204b7d6a71
Raymond Ottun <sayjava@gmail.com>
2023-01-07 09:57:05 +0000

    feat: pagination

--- dashboard/src/components/mocks/Table.vue
+++ dashboard/src/components/mocks/Table.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-table
-      :data="mocks"
+      :data="items"
       @selection-change="handleSelectionChange"
       stripe
       border
@@ -34,16 +34,38 @@
           <status-column :response="scope.row.response" />
         </template>
       </el-table-column>
+      <template #empty>
+        <div>        
+         No registered mocks
+        </div>
+      </template>
     </el-table>
+    <div class="page">
+      <el-pagination
+        layout="prev, pager, next"
+        :total="mocks.length"
+        background
+        hide-on-single-page
+        v-model:current-page="currentPage"
+        v-model:page-size="pageSize"
+      />
+    </div>
   </div>
 </template>
 <script lang="ts">
 import StatusColumn from "@/components/row/Status.vue";
 import ExpandRow from "@/components/row/Expand.vue";
+import { ref } from "vue";
 
 export default {
   props: ["mocks"],
   emits: ["selections"],
+  setup() {
+    return {
+      currentPage: ref(1),
+      pageSize: ref(15),
+    };
+  },
   components: {
     StatusColumn,
     ExpandRow,
@@ -53,10 +75,23 @@ export default {
       this.$emit("selections", mocks);
     },
   },
+  computed: {
+    items() {
+      const start = (this.currentPage - 1) * this.pageSize;
+      return this.mocks.slice(start, start + this.pageSize);
+    },
+  },
 };
 </script>
 <style scoped>
 .table {
   width: 100%;
+}
+
+.page {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  padding: 12px 0;
 }
 </style>
--- dashboard/src/components/records/Table.vue
+++ dashboard/src/components/records/Table.vue
@@ -2,7 +2,7 @@
   <div>
     <el-table
       @selection-change="handleSelectionChange"
-      :data="items"
+      :data="records"
       stripe
       border
       style="width: 100%"
@@ -35,7 +35,22 @@
           <Method :request="scope.row.request" />
         </template>
       </el-table-column>
+      <template #empty>
+        <div class="no-records">
+          No recorded requests
+        </div>
+      </template>
     </el-table>
+    <div class="page">
+      <el-pagination
+        layout="prev, pager, next"
+        :total="items.length"
+        background
+        hide-on-single-page
+        v-model:current-page="currentPage"
+        v-model:page-size="pageSize"
+      />
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -43,8 +58,21 @@ import ExpandRow from "@/components/row/Expand.vue";
 import Status from "@/components/row/Status.vue";
 import Method from "@/components/row/Method.vue";
 import Timestamp from "@/components/row/Timestamp.vue";
+import { ref } from "vue";
 export default {
   props: ["items", "columns"],
+  setup() {
+    return {
+      currentPage: ref(1),
+      pageSize: ref(20),
+    };
+  },
+  computed: {
+    records() {
+      const start = (this.currentPage - 1) * this.pageSize;
+      return this.items.slice(start, start + this.pageSize);
+    },
+  },
   methods: {
     handleSelectionChange: (records: any[]) => {
       console.log("Selected items", records);
@@ -56,3 +84,17 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.page {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  padding: 12px 0;
+}
+
+.no-records {
+  font-size: 600;
+  font-weight: 600;
+}
+</style>
